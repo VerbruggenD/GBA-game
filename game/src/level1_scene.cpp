cd29e333002c3c08a2e94dbd143fa72b0415d602
D3admous3 <43110723+D3admous3@users.noreply.github.com>
2022-01-03 23:35:28 +0100

    opgekuist

--- game/src/level1_scene.cpp
+++ game/src/level1_scene.cpp
@@ -7,11 +7,9 @@
 
 #include "level1_scene.h"
 
-#include "level1_bg.h"
-
 std::vector<Background *> Level1Scene::backgrounds() {
     return {
-        bg.get()
+        //bg.get()
     };
 }
 
@@ -27,7 +25,7 @@ void Level1Scene::load() {
     //engine.get()->enableText();
 
     //foregroundPalette = std::unique_ptr<ForegroundPaletteManager>(new ForegroundPaletteManager(testPal, sizeof(testPal)));
-    backgroundPalette = std::unique_ptr<BackgroundPaletteManager>(new BackgroundPaletteManager(level1_bgPal, sizeof(level1_bgPal)));
+    //backgroundPalette = std::unique_ptr<BackgroundPaletteManager>(new BackgroundPaletteManager(level1_bgPal, sizeof(level1_bgPal)));
 
     SpriteBuilder<Sprite> builder;
 
@@ -41,39 +39,13 @@ void Level1Scene::load() {
     
     TextStream::instance().setText("TEEESST", 0, 0);
 
-    bg = std::unique_ptr<Background>(new Background(1, level1_bgTiles, sizeof(level1_bgTiles), level1_bgMap, sizeof(level1_bgMap)));
-    bg.get()->useMapScreenBlock(16);
-
-    //TextStream::instance().setText(std::string("Portal 2.5"), 5, 8);
+    //bg = std::unique_ptr<Background>(new Background(1, level1_bgTiles, sizeof(level1_bgTiles), level1_bgMap, sizeof(level1_bgMap)));
+    //bg.get()->useMapScreenBlock(16);
 
     engine->getTimer()->start();
 }
 
  void Level1Scene::tick(u16 keys) {
-     TextStream::instance().setText(engine->getTimer()->to_string(), 18, 1);
+     //TextStream::instance().setText(engine->getTimer()->to_string(), 18, 1);
 
-//     if(pressingAorB && !((keys & KEY_A) || (keys & KEY_B))) {
-//         engine->getTimer()->toggle();
-//         pressingAorB = false;
-//     }
-
-//     if(keys & KEY_START) {
-//         if(!engine->isTransitioning()) {
-//             //engine->enqueueSound(zelda_secret_16K_mono, zelda_secret_16K_mono_bytes);
-
-//             //TextStream::instance() << "entered: starting next scene";
-
-//             //engine->transitionIntoScene(new FlyingStuffScene(engine), new FadeOutScene(2));
-//         }
-//     } else if(keys & KEY_LEFT) {
-//         animation->flipHorizontally(true);
-//     } else if(keys & KEY_RIGHT) {
-//         animation->flipHorizontally(false);
-//     } else if(keys & KEY_UP) {
-//         animation->flipVertically(true);
-//     } else if(keys & KEY_DOWN) {
-//         animation->flipVertically(false);
-//     } else if((keys & KEY_A) || (keys & KEY_B)) {
-//         pressingAorB = true;
-//     }
 }
