1a1d908bb27c3d6100e7fad88b34114460c1c2ef
D3admous3 <43110723+D3admous3@users.noreply.github.com>
2022-01-21 12:13:10 +0100

    added barn

---
+++ game/src/buildings/barn.cpp
@@ -0,0 +1,1 @@
+#include "barn.h"
---
+++ game/src/buildings/barn.h
@@ -0,0 +1,14 @@
+#ifndef GBA_GAME_BUILDING_BARN
+#define GBA_GAME_BUILDING_BARN
+
+#include <libgba-sprite-engine/gba_engine.h>
+#include <libgba-sprite-engine/gba/tonc_memmap.h>
+
+class Barn {
+private:
+    
+public:
+
+}
+
+#endif //GBA_GAME_BUILDING_BARN
