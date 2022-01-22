8deafd280494bea46186e1b49d6e32c3f69c347c
D3admous3 <43110723+D3admous3@users.noreply.github.com>
2022-01-22 21:30:52 +0100

    changed spritesize

--- game/src/field/crops.cpp
+++ game/src/field/crops.cpp
@@ -4,7 +4,7 @@
 Crop::Crop(SpriteBuilder<Sprite> builder, int x, int y, unsigned char whichCrop) {
     this->CropSprite = builder
         .withLocation(x,y)
-        .withSize(SIZE_16_16)
+        .withSize(SIZE_32_32)
         .withData(CropsTiles, sizeof(CropsTiles))
         .withinBounds()
         .buildPtr();
