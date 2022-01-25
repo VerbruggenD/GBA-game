6758f8432e4f7c13864bbfa6f0feb3db38cee17c
DivadGe <david.gerits@student.uhasselt.be>
2022-01-25 15:08:48 +0100

    crops pxls

--- game/src/field/crops_data.h
+++ game/src/field/crops_data.h
@@ -3,13 +3,14 @@
 
 //======================================================================
 //
-//	Crops, 32x192@8, 
-//	Transparent color : 00,00,00
-//	+ 96 tiles not compressed
-//	Total size: 6144 = 6144
+//	Crops, 32x224@8, 
+//	+ palette 256 entries, not compressed
+//	+ 77 tiles (t|f|p reduced) not compressed
+//	+ regular map (flat), not compressed, 4x28 
+//	Total size: 512 + 4928 + 224 = 5664
 //
-//	Time-stamp: 2022-01-22, 21:28:15
-//	Exported by Cearn's GBA Image Transmogrifier, v0.8.16
+//	Time-stamp: 2022-01-25, 13:55:13
+//	Exported by Cearn's GBA Image Transmogrifier, v0.8.6
 //	( http://www.coranac.com/projects/#grit )
 //
 //======================================================================
@@ -17,8 +18,14 @@
 #ifndef GRIT_CROPS_H
 #define GRIT_CROPS_H
 
-#define CropsTilesLen 6144
-extern const unsigned int CropsTiles[1536];
+#define CropsTilesLen 4928
+extern const unsigned short CropsTiles[2464];
+
+#define CropsMapLen 224
+extern const unsigned short CropsMap[112];
+
+#define CropsPalLen 512
+extern const unsigned short CropsPal[256];
 
 #endif // GRIT_CROPS_H
 
